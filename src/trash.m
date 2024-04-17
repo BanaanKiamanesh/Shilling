474f6c66ab024122cc5c74c053f5777374d1b43e
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2024-04-17 23:41:02 +0330

    RKSSP33/RKSSP53 Implementation

--- src/trash.m
+++ src/trash.m
@@ -4,9 +4,9 @@ clc
 
 f = @(t, x) [x(2); (1 - x(1)^2)*x(2) - x(1)]; % Van der Pol ODE
 TSpan= [0, 20];    % Time span
-Y0= [2; 0];        % Initial condition
+Y0= [2; 2];        % Initial condition
 h = 0.01;          % Step size
-[T, Y] = odeRK3(f, TSpan, Y0, h);
+[T, Y] = odeRKSSP53(f, TSpan, Y0, h);
 
 % Plot results
 figure;
