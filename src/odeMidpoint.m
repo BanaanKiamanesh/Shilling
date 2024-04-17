1033e4889469f33cf653681ebb187e8de658652e
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2024-04-17 22:44:11 +0330

    MidPoing ODE Solver Implemented

---
+++ src/odeMidpoint.m
@@ -0,0 +1,59 @@
+function [Time, Y] = odeMidpoint(ODEfun, TSpan, Y0, h)
+    %ODEMIDPOINT Method Implementation
+    % Inputs:
+    %   ODEfun: function handle for the ODE
+    %   TSpan: time span as [t0, tf]
+    %   Y0: initial condition as a column vector (default: zeros)
+    %   h: step size (default: 0.01)
+    %
+    % Example Usage:
+    %   f = @(t, x) [x(2); (1 - x(1)^2)*x(2) - x(1)]; % Van der Pol ODE
+    %   TSpan= [0, 20];    % Time span
+    %   Y0= [2; 0];        % Initial condition
+    %   h = 0.01;          % Step size
+    %   [T, Y] = odeMidpoint(f, TSpan, Y0, h);
+    %
+    %   % Plot results
+    %   figure;
+    %   plot(T, Y, 'LineWidth', 2);
+    %   xlabel('Time [s]');
+    %   ylabel('State Variables');
+    %   title('Van der Pol Equation');
+    %   grid on;
+    %   axis([0, 20, -3, 3]);
+
+    % Set default values if not provided
+    if nargin < 4
+        h = 0.01;
+    end
+
+    t0 = TSpan(1);
+    tf = TSpan(2);
+
+    % Initial setup
+    t = t0;
+    y = Y0;
+
+    % Preallocate arrays to store values
+    StepNum = ceil((tf - t0) / h);
+    Time = zeros(StepNum, 1);
+    Y = zeros(StepNum, length(Y0));
+    Time(1) = t;
+    Y(1, :) = y';
+
+    % Main loop
+    idx = 1;
+    while t < tf
+        % Midpoint method
+        k1 = ODEfun(        t,            y);
+        k2 = ODEfun(t + 0.5*h, y + 0.5*h*k1);
+
+        y = y + h * k2;
+        t = t + h;
+
+        % Store values
+        idx = idx + 1;
+        Time(idx) = t;
+        Y(idx, :) = y';
+    end
+end
