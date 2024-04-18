c6767500e697741c34d390820b22e5198f4f943b
Banaan Kiamanesh <kiamaneshbanaan@gmail.com>
2024-04-18 14:31:56 +0330

    RKZ10/RKH10/RKO10 Implemented

--- src/trash.m
+++ src/trash.m
@@ -4,9 +4,9 @@ clc
 
 f = @(t, x) [x(2); (1 - x(1)^2)*x(2) - x(1)]; % Van der Pol ODE
 TSpan= [0, 20];    % Time span
-Y0= [0; 2];        % Initial condition
+Y0= [2; 0];        % Initial condition
 h = 0.01;          % Step size
-[T, Y] = odeRKCV8(f, TSpan, Y0, h);
+[T, Y] = odeRKH10(f, TSpan, Y0, h);
 
 % Plot results
 figure;
